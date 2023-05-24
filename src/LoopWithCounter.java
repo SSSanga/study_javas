f7f3295b260d833c385a5217b67019d3293a8b1a
SSSanga <ksa901125@gmail.com>
2023-05-24 11:04:37 +0900

    while 문의 조건을 비교 부호가 아니라 같이 않음으로 변경함.

--- src/LoopWithCounter.java
+++ src/LoopWithCounter.java
@@ -2,7 +2,7 @@ public class LoopWithCounter {
     public static void main(String[] args) {
         int number = 1;
         int numberend= 6;
-        while (number < numberend) {
+        while (number != numberend) {
             System.out.println (number);
             number = number + 1;
 
