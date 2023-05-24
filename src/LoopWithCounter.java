974919d580888c5dbf50faac1e38a04385d3c6da
SSSanga <ksa901125@gmail.com>
2023-05-24 10:57:55 +0900

    number를 +1로 증가하게 while loop를 돌았음.

---
+++ src/LoopWithCounter.java
@@ -0,0 +1,13 @@
+public class LoopWithCounter {
+    public static void main(String[] args) {
+        int number = 1;
+        int numberend= 6;
+        while (number < numberend) {
+            System.out.println (number);
+            number = number + 1;
+
+        }
+        System.out.println ("END");
+        // return 0 ;
+    }
+}
