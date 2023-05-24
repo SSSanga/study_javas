5eeda9db1b8fb6a3e210e989e5aaaba5c2ce642b
SSSanga <ksa901125@gmail.com>
2023-05-24 17:00:32 +0900

    2차 for문의 if문에서 second ==first 물어도 true이므로  break!

--- src/LoopNestedLoopstest.java
+++ src/LoopNestedLoopstest.java
@@ -5,7 +5,7 @@ public class LoopNestedLoopstest {
             System.out.println("for " +  first);
         for (int second = 1; second <= 4;second = second +1) {
             System.out.print ("_?");
-            if (second >= first) { break;}
+            if (second == first) { break;}
              }
           System.out.println ();}
         
