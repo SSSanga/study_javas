025d5395bf53815ffa30b2c1e88e8e3cb2a39a4d
SSSanga <ksa901125@gmail.com>
2023-05-24 16:39:26 +0900

    marker도 같이 넣을려고 함.

---
+++ src/LoopNestedLoopstest.java
@@ -0,0 +1,18 @@
+public class LoopNestedLoopstest {
+    public static void main(String[] args) {
+        
+        for (int first = 1; first < 5; first = first + 1) {
+            System.out.println("for " +  first);
+        for (int second = 1; second <= 4;second = second +1) {
+            System.out.print ("_?");
+            if (second >= first) { break;}
+             }
+          System.out.println ();}
+        
+        System.out.println ("END");
+    } 
+       
+        
+               
+        // return 0 ;
+    }
