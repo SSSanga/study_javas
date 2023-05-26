7ce9ed29bb34ce97fce2d6b5d544a50e8ce9da16
SSSanga <ksa901125@gmail.com>
2023-05-26 09:48:05 +0900

    5월 25일 class end_덜 끝냄요.

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,13 +1,31 @@
 public class Arrays {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        
-        for (int name = 0; name < 4 ; name = name + 1) {
-            System.out.print (cars [name] +" " );
+        String [][] numbers = {{"10", "20", "30", "40"} ,
+                              {"50", "60", "70"}};
+       for (int second = 0; second < numbers.length;second = second +1){
+        for (int third = 0; third < numbers[second].length;third=third +1){
+            System.out.print (numbers[second][third]+ ", ");
         }
-        // return 0 ;
-        // for문으로 array값 출력하기.     
-    }
-    
-}
+        System.out.println();
 
+       }
+           String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+           System.out.println (cars);
+       }
+
+    // String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    // System.out.println (cars.length);
+
+    // for (int first = 0; first < cars.length ; first = first + 1) {
+    // System.out.print (cars [first]+",");
+    // }
+
+    // System.out.println ();
+
+    // for문으로 array값 출력하기.
+    // for (int name = 0; name < 4 ; name = name + 1) {
+    // System.out.print (cars [name] +" " );
+
+    // return 0 ;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner (System.in);
         //  refer _ https://www.acmicpc.net/problem/10998
-        // 문제 : A x B
+        // 문제 : A / B
         // 입력: 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-        
-        int A = 0; int B = 0;
-        A = myObj.nextInt ();
-        B = myObj.nextInt ();
+        //
+        Double A = 0d; Double B = 2d;
+        A = myObj.nextDouble ();
+        B = myObj.nextDouble ();
 
-        System.out.print (A * B);
+        System.out.print (A / B);
 
             // return 0 ;
     }
