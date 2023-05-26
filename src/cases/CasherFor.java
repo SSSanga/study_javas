d140fc3c54e8feb3b522f64dd50880f3b6244f95
SSSanga <ksa901125@gmail.com>
2023-05-26 17:06:39 +0900

    일단 뭐가 문제인지 모르겠으나 vscode 오류가 났으므로 update하고 껐다 킨다.

--- src/cases/CasherFor.java
+++ src/cases/CasherFor.java
@@ -1,29 +1,50 @@
 package cases;
 
 import java.util.Scanner;
+
 public class CasherFor {
     public static void main(String[] args) {
         try {
-            System.out.println ("장바구니 5개까지 가능");
-            System.out.println ("===계산 시작===");
-
-            System.out.println ("1번 가격 : 10");
-            System.out.println ("2번 가격 : 20");
-            System.out.println ("3번 가격 : 1000");
-            System.out.println ("4번 가격 : 50");
-            System.out.println ("5번 가격 : 70");
-
+            Scanner myObj = new Scanner(System.in);
+            int price = 0;
+            System.out.println("장바구니 5개까지 가능");
+            System.out.println("===계산 시작===");
+            // 1. 금액이 100을 초과하면 0원을 적용한다는 true와 break를 건다.
             
-
+            System.out.print("1번 가격 :");
+            price = myObj.nextInt();
+            System.out.print("2번 가격 : ");
+            price = myObj.nextInt();
+            System.out.print("3번 가격 : ");
+            price = myObj.nextInt();
+            System.out.print("4번 가격 : ");
+            price = myObj.nextInt();
+            System.out.print("5번 가격 : ");
+            price = myObj.nextInt();
+            if (price > 100) {
+                price = price * 0;
+            }
+            // System.out.println ("1번 가격 :");
+            // price = myObj.nextInt();
+            // System.out.println ("2번 가격 : ");
+            // price = myObj.nextInt();
+            // System.out.println ("3번 가격 : ");
+            // price = myObj.nextInt();
+            // System.out.println ("4번 가격 : ");
+            // price = myObj.nextInt();
+            // System.out.println ("5번 가격 : ");
+            // price = myObj.nextInt();
 
             System.out.println("---담은 총합---");
             System.out.println("int 1번 + 2번+3번+4번+5번");
-            
+
         } catch (Exception e) {
             // TODO: handle exception
-        }finally {};
-            System.out.println ();
-            System.out.println ("---계산종료---");
+        } finally {
+        }
+        ;
+        System.out.println();
+        System.out.println("---계산종료---");
         // return 0;
     }
 }
