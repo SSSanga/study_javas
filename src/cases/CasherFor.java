5a4aeed21598030da40bf61540414c988394835f
SSSanga <ksa901125@gmail.com>
2023-05-26 16:44:34 +0900

    1차: 직접 입력 후 print로 출력

---
+++ src/cases/CasherFor.java
@@ -0,0 +1,29 @@
+package cases;
+
+import java.util.Scanner;
+public class CasherFor {
+    public static void main(String[] args) {
+        try {
+            System.out.println ("장바구니 5개까지 가능");
+            System.out.println ("===계산 시작===");
+
+            System.out.println ("1번 가격 : 10");
+            System.out.println ("2번 가격 : 20");
+            System.out.println ("3번 가격 : 1000");
+            System.out.println ("4번 가격 : 50");
+            System.out.println ("5번 가격 : 70");
+
+            
+
+
+            System.out.println("---담은 총합---");
+            System.out.println("int 1번 + 2번+3번+4번+5번");
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }finally {};
+            System.out.println ();
+            System.out.println ("---계산종료---");
+        // return 0;
+    }
+}
