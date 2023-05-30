41717578fd697c492097287653748257c6e4c03f
SSSanga <ksa901125@gmail.com>
2023-05-30 15:37:24 +0900

    일단 강사님꺼 version

--- src/MethodBasic.java
+++ src/MethodBasic.java
@@ -5,8 +5,42 @@ public class MethodBasic {
     // //body
     // }
     // x + 1
-    public int F(int x) {
-        int results = x + 1;
+    // 강사님 버전...!!!!!!!!!!!!!!!!!
+            // public class MethodBasics {
+            //     // <access_modifier> <return_type> <method_name>( list_of_parameters)
+            //     // {
+            //     // //body
+            //     // }
+            //     // x에 +1
+            //     public int F(int x) // x = 0
+            //     {
+            //         int result = x + 1;
+            //         return result;
+            //     }
+            //     public static void main(String[] args) {
+            //         try {
+            //             int x = 4;
+            //             int y = 0;
+            //             // y = x + 1; // 변수에 + 1
+            //             // y = F(x);
+            //             MethodBasics methodBasics = new MethodBasics();
+            //             y = methodBasics.F(x);
+            
+            //             y = y + 1; // 변수에 + 1
+            //             MethodBasics methodBasics_first = new MethodBasics();
+            //             y = methodBasics_first.F(y);  // F(0)
+            //             y = methodBasics_first.F(y);  // F(0)
+            
+            //         } catch (Exception e) {
+            //             // TODO: handle exception
+            //         }
+            //         System.out.println();
+            //         // return 0;
+            //     }
+            // } 
+    public int F(int x) //x = 4 , call by value
+     { 
+        int results = x + 1; 
         return results;
         // x + 1
     }
@@ -36,10 +70,19 @@ public class MethodBasic {
             //
 
             y = y + 1; // 변수에 +1
+            MethodBasic methodBasic_first = new MethodBasic();
+            y = methodBasic_first.F(0);
+            y = methodBasic_first.F(y);
+            //이렇게 되면 위에서 x = "y=0"인 0이 날아감. 
+            //이렇게 되면 results= 0 + 1
+            // return results = 1
 
-            MethodBasic Calculate =new MethodBasic ();
-            y = Calculate.Y(y);
+            // MethodBasic Calculate =new MethodBasic ();
+            // y = Calculate.Y(y);
+            // y = methodBasic.F(y); 
+            
             // y = Y (y);
+
         } catch (Exception e) {
             // TODO: handle exception
         }
