ac4e2f3d77d25c358828a5dca428f979e5894ec7
SSSanga <ksa901125@gmail.com>
2023-05-31 11:44:43 +0900

    '_'B 개발자 입니다. first = 2 입니다.

---
+++ src/GitConflicts.java
@@ -0,0 +1,14 @@
+public class GitConflicts {
+    public static void main(String[] args) {
+        try {
+            //I am B 개발자 '_'
+            
+            int first = 2;
+            first = first + 1;
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0 ;
+    }
+}
